d2eab1fd54a28f3a4a425bb272e1fdac43da7c53
gonzagrau <ggrau@itba.edu.ar>
2024-07-22 13:45:00 -0300

    preliminary plot by cluster

--- first_test.py
+++ first_test.py
@@ -5,11 +5,11 @@ import seaborn as sns
 
 def main():
     DATA_PATH = r'../dataset/GSM4768020_CT02_20190528_AR05.csv'
-    df = pd.read_csv(DATA_PATH, index_col=0)
-    print(df.head(10))
-    ax = sns.heatmap(data=df.head(10))
-    ax.set_title('Expresion de gen en neurona')
-    plt.show()
+    ANNOT_PATH = r'../clock_neurons_annotation.csv'
+    genexp_df = pd.read_csv(DATA_PATH, index_col=0).T
+    print(genexp_df.head(10))
+    annot_df = pd.read_csv(ANNOT_PATH, index_col=0)
+    genexp_df['cluster'] = annot_df['Idents']
 
 
 if __name__ == '__main__':
